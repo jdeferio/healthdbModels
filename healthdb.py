ada11bc52105f6991261e60db10d4b8f0f45dbd2
Joseph Deferio <j.deferio@gmail.com>
2021-01-21 10:50:38 -0500

    update models and back_population

--- healthdb.py
+++ healthdb.py
@@ -60,14 +60,14 @@ class Encounter(Base):
     id = Column(UUID, primary_key=True, default=uuid.uuid4())
     start_date = Column(DateTime)
     end_date = Column(DateTime)
-    patient_id = Column(UUID, ForeignKey("patient.id"), index=True)
-    organization_id = Column(UUID, ForeignKey("organization.id"), index=True)
-    provider_id = Column(UUID, ForeignKey("provider.id"), index=True)
-    payer_id = Column(UUID, ForeignKey("payer.id"), index=True)
+    patient_id = Column(UUID, ForeignKey("patient.id"))
+    external_id = Column(String)
     enc_class = Column(String)
     enc_code = Column(String(9))
     enc_description = Column(String)
-    condition_id = Column(UUID, ForeignKey("condition.id"), index=True)
+    condition_id = Column(UUID, ForeignKey("condition.id"))
+
+    conditions = relationship("Condition", back_populates="encounter")
 
 
 class Organization(Base):
@@ -98,9 +98,8 @@ class Provider(Base):
     __tablename__ = "provider"
 
     id = Column(UUID, primary_key=True, default=uuid.uuid4())
-    organization_id = Column(
-        UUID, ForeignKey("organization.id"), index=True
-    )
+    organization_id = Column(UUID, ForeignKey("organization.id"))
+    encounter_id = Column(UUID, ForeignKey("encounter.id"))
     name = Column(String)
     gender = Column(String)
     specialty = Column(String)
@@ -109,14 +108,15 @@ class Provider(Base):
     state = Column(String(2))
     zipcode = Column(String(5))
 
+    organization = relationship("Organization", backref="provider")
+
 
 class Imaging(Base):
     __tablename__ = "imaging"
 
     id = Column(UUID, primary_key=True, default=uuid.uuid4())
     study_date = Column(DateTime)
-    patient_id = Column(UUID, ForeignKey("patient.id"), index=True)
-    encounter_id = Column(UUID, ForeignKey("encounter.id"), index=True)
+    encounter_id = Column(UUID, ForeignKey("encounter.id"))
     modality = Column(String)
     modality_description = Column(String)
     body_part_code = Column(String)
@@ -129,8 +129,14 @@ class Condition(Base):
     __tablename__ = "condition"
 
     id = Column(UUID, primary_key=True, default=uuid.uuid4())
-    code = Column(String, ForeignKey("encounter.code"), unique=True)
+    start_date = Column(DateTime)
+    end_date = Column(DateTime)
+    patient_id = Column(UUID, ForeignKey("patient.id"))
+    encounter_id = Column(UUID, ForeignKey("encounter.id"))
+    code = Column(String, unique=True)
     description = Column(String)
+
+    encounter = relationship("Encounter", back_populates="conditions")
 
 
 class Medication(Base):
@@ -139,12 +145,13 @@ class Medication(Base):
     id = Column(UUID, primary_key=True, default=uuid.uuid4())
     start_date = Column(DateTime)
     end_date = Column(DateTime)
-    patient_id = Column(UUID, ForeignKey("patient.id"), index=True)
-    encounter_id = Column(UUID, ForeignKey("encounter.id"), index=True)
-    payer_id = Column(UUID, ForeignKey("payer.id"), index=True)
+    encounter_id = Column(UUID, ForeignKey("encounter.id"))
+    payer_id = Column(UUID, ForeignKey("payer.id"))
     med_code = Column(String)
     med_description = Column(String)
-    condition = Column(String, ForeignKey("condition.code"), index=True)
+    condition = Column(String, ForeignKey("condition.code"))
+
+    payer = relationship("Payer", back_populates="medication")
 
 
 class Procedure(Base):
@@ -152,8 +159,8 @@ class Procedure(Base):
 
     id = Column(UUID, primary_key=True, default=uuid.uuid4())
     date = Column(DateTime)
-    patient_id = Column(UUID, ForeignKey("patient.id"), index=True)
-    encounter_id = Column(UUID, ForeignKey("encounter.id"), index=True)
+    patient_id = Column(UUID, ForeignKey("patient.id"))
+    encounter_id = Column(UUID, ForeignKey("encounter.id"))
     proc_code = Column(String)
     proc_description = Column(String)
-    condition = Column(String, ForeignKey("condition.code"), index=True)
+    condition = Column(String, ForeignKey("condition.code"))
